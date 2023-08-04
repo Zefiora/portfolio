7db947db4a6ac6ddd802624df5a5f35acdc84c51
Matthew Barnes <sakume@rocketmail.com>
2023-08-04 12:14:37 -0500

    modified:   about-me.html 	modified:   css/sassy-aboutme.css.map 	modified:   css/sassy-aboutme.scss 	new file:   css/sassy-portfolio.css 	new file:   css/sassy-portfolio.css.map 	new file:   css/sassy-portfolio.scss 	modified:   css/sassy.css 	modified:   css/sassy.css.map 	modified:   css/sassy.scss 	modified:   index.html 	new file:   javascript/javascript-5.js 	modified:   job-history.html 	modified:   portfolio.html 	modified:   skills.html

---
+++ javascript/javascript-5.js
@@ -0,0 +1,42 @@
+const items = document.querySelectorAll('.pagesec')
+const options = {
+  threshold: 0.1
+}
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}, options)
+items.forEach(item => {
+  observer.observe(item);
+})
+
+function arryvalidation() {
+    let x = document.getElementById("textarry").value;
+    let text;
+    let i;
+    let arryz;
+    arryz = x.split(',');
+    arryz = arryz.sort(function(a, b) {
+        return a - b;
+      });
+        if (arryz.length > 10) {
+            alert("You've entered to many numbers. Please enter 10 random numbers separated by commas into the input field.");
+            return;
+        } else if (arryz.length <= 9) {
+            alert("You haven't entered enough numbers. Please enter 10 random numbers separated by commas into the input field.");
+            return;
+        };
+        for (i of arryz){
+            i = Number(i);
+              if (isNaN(i)){
+                alert("One of your values is not a number. Enter 10 random numbers separated by commas into the input field.");
+                return;
+              } else {
+                text += "\n" + i;
+              };
+        };
+        alert(text);
+    }
